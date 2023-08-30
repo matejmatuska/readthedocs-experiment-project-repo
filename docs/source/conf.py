4fbd6acb44f3d31097df74cb4b84758fe965e408
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2023-08-30 17:52:50 +0200

    rename project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Repository'
+copyright = '2021, Whatever'
+author = 'Whatever'
 
 release = '0.1'
 version = '0.1.0'
